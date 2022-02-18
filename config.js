60838fd161df12ad063eee200bbf644cd5970cad
danvogiavinh <99949352+danvogiavinh@users.noreply.github.com>
2022-02-18 22:21:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'Mẹ';
 const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-10';
 const titleCard = 'Đôi lời con muốn nhắn với mẹ';
-const contentCard = 'Vậy là đã đến ngày 8/3 rồi. Con cũng chẳng biết nói gì nữa. Mẹ quá tuyệt vời, không ngôn từ nào có thể diễn tả được hết.Con chỉ muốn nói rằng:Chúc mẹ có một ngày 8/3 thật vui vẻ, chúc cô ngày càng xinh đẹp, thành công hơn nữa trong cuộc sống.Cảm ơn mẹ đã đồng hành cùng với con suốt thời gian qua!';
+const contentCard = 'Vậy là đã đến ngày 8/3 rồi. Con cũng chẳng biết nói gì nữa. Mẹ quá tuyệt vời, không ngôn từ nào có thể diễn tả được hết.Con chỉ muốn nói rằng:Chúc mẹ có một ngày 8/3 thật vui vẻ, chúc mẹ ngày càng xinh đẹp, thành công hơn nữa trong cuộc sống.Cảm ơn mẹ đã đồng hành cùng với con suốt thời gian qua!';
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
